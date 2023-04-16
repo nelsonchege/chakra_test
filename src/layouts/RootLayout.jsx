5a986ab2ca2bdbd91ee3a91ab7875aea2b3cd359
nelson <nelson3chege@gmail.com>
2023-04-16 18:22:28 +0300

    update:add responsive design

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router-dom";
 import { Grid, GridItem } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 export default function RootLayout() {
   return (
     <Grid templateColumns="repeat(6, 1fr)" bg={"gray.50"}>
-      <GridItem as="aside" colSpan="1" bg={"purple.400"} minHeight="100vh">
+      <GridItem
+        as="aside"
+        colSpan={{ base: 6, lg: 2, xl: 1 }}
+        bg={"purple.400"}
+        minHeight={{ lg: "100vh" }}
+      >
         <span>sidebar</span>
       </GridItem>
-      <GridItem as="main" colSpan="5">
+      <GridItem as="main" colSpan={{ base: 6, lg: 4, xl: 5 }}>
         <Navbar />
         <Outlet />
       </GridItem>
