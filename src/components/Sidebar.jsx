1f224a9de18b9044ddd985d6d452d69a033468ac
nelson <nelson3chege@gmail.com>
2023-04-16 18:54:37 +0300

    update:add chakra icons

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import { AtSignIcon, CalendarIcon, EditIcon } from "@chakra-ui/icons";
+import { List, ListIcon, ListItem } from "@chakra-ui/react";
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Sidebar() {
+  return (
+    <List color={"white"} fontSize={"1.2em"} spacing={4}>
+      <ListItem>
+        <ListIcon as={CalendarIcon} color={"white"} />
+        <NavLink to={"/"}>DashBoard</NavLink>
+      </ListItem>
+      <ListItem>
+        <ListIcon as={EditIcon} color={"white"} />
+        <NavLink to={"/create"}>New task</NavLink>
+      </ListItem>
+      <ListItem>
+        <ListIcon as={AtSignIcon} color={"white"} />
+        <NavLink to={"/profile"}>Profile</NavLink>
+      </ListItem>
+    </List>
+  );
+}
